893ed42f34de1c8b89de748b8278af4e0e2298ed
Ferguson <fs11239@mmg.marinegroup.com>
2017-01-12 08:46:12 -0600

    Second Commit

---
+++ bkup/global_reclass.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fs11239
+ * Date: 1/6/2017
+ * Time: 2:52 PM
+ */
---
+++ monthly_processing/cur_month_setup/cross_hull.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fs11239
+ * Date: 1/9/2017
+ * Time: 10:51 AM
+ */

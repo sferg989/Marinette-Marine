4b7cac9da35deecaf2eeac4a6838a69aa47a4456
Stephen Ferguson <stephen.ferguson@fincantieri.us.com>
2017-01-26 07:45:43 -0600

    Working copy ca compare

---
+++ monthly_processing/load_baseline/workers/load_bcr.js
@@ -0,0 +1,29 @@
+/**
+ * Created by fs11239 on 1/5/2017.
+ */
+self.addEventListener('message', function(e) {
+    self.postMessage(e.data);
+
+}, false);
+this.onmessage = function(e) {
+
+    var action     = "bcr_check";
+    var code       = e.data.code;
+    var rpt_period = e.data.rpt_period;
+    var data       = escape(e.data.bcrData2);
+    var http       = new XMLHttpRequest();
+
+    var url = "../load_baseline.php";
+    var params = "control="+action+"&bcr_data="+data+"&rpt_period="+rpt_period+"&code="+code+"";
+    http.open("POST", url, true);
+
+//Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            postMessage({id: action});
+        }
+    }
+    http.send(params);
+};

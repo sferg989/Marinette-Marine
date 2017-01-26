3c848d0740ab6c731254874ba3b23cb21fd61367
Stephen Ferguson <stephen.ferguson@fincantieri.us.com>
2017-01-26 09:15:34 -0600

    working bootbox.confirm

--- login.php
+++ login.php
@@ -8,62 +8,137 @@ function insertNewUser($username,$password){
     return false;
 }
 function checkUser($username,$password){
-    $sql    = "select role, pw from fmm.evms where username = '$username'";
+    $sql    = "select user_name, role, pw from fmm_evms.user where user_name = '$username'";
+
     $rs     = dbCall($sql, "fmm_evms");
     $msg = "";
-    if($rs!="")
-    {
-        $role   = $rs->fields["role"];
-        $pw     = $rs->fields["pw"];
+    $user_name = $rs->fields["user_name"];
+    $role = $rs->fields["role"];
+    $pw = $rs->fields["pw"];
 
-        $decode = base64_decode($pw);
-        if($decode!=$password){
-            $msg = "pw_incorrect";
-        }
-        else{
-            $msg = "login";
-        }
+    if($user_name ==""){
+        $msg = "user_did_not_match";
+        return $msg;
     }
-    else{
-        $msg = "first_time";
-
+    $decode = base64_decode($pw);
+    if($decode!=$password){
+        $msg = "pw_incorrect";
+        return $msg;
     }
-    return $msg;
 }
-print $username;
-print "<br>";
-print $password;
-print "this worked";
+$z = "
+<html>
+<head>
+";
+$z.= "
+    <script
+            src=\"https://code.jquery.com/jquery-2.2.4.min.js\"
+            integrity=\"sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=\"
+            crossorigin=\"anonymous\"></script>    
+    <script src=\"https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js\"></script>
+    <script src=\"https://npmcdn.com/bootstrap@4.0.0-alpha.2/dist/js/bootstrap.js\"></script>
+    <script src=\"inc/lib/css/bootbox.min.js\"></script>
+    <link rel=\"stylesheet\" href=\"inc/lib/css/bootstrap/css/bootstrap.css\">
+";
+$z.="</head>";
+$z.="<body>";
 $result = checkUser($username,$password);
-print "
+
+$z.= "
     <script>
     function navigate(url){
         window.location.href = url;
     }
     
-    
     ";
 switch ($result) {
     case "pw_incorrect":
 
-        print "navigate('login.html')</script>";
+        $z.="alert('Password Incorrect'); 
+                navigate('login.html');</script>";
         break;
-    case "login":
-        $_SESSION["user_name"] = $username;
-        print "navigate('login.html')</script>";
+    case "user_did_not_match":
+        //$_SESSION["user_name"] = $username;
+        $z.="bootbox.confirm({
+    message : 'Is this the first time you are logging into EV?',
+    buttons : {
+        confirm: {
+            label    : 'Yes',
+            className: 'btn-success'
+        },
+        cancel : {
+            label    : 'No',
+            className: 'btn-danger'
+        }
+    },
+    callback: function (result) {
+        console.log('This was logged in the callback: ' + result);
+        if (result == false) {
+            bootbox.alert(\"Please try again\", 
+            function(){ navigate('login.html');});
+        }
+        if (result == 'yes') {
+            console.log('yesss');
+            bootbox.form({
+    title: 'User details',
+    fields: {
+        name: {
+            label: 'Name',
+            value: 'John Connor',
+            type:  'text'
+        },
+        email: {
+            label: 'E-mail',
+            type:  'email',
+            value: 'johnconnor@skynet.com'
+        },
+        type: {
+            label: 'Type',
+            type:  'select',
+            options: [
+                {value: 1, text: 'Human'},
+                {value: 2, text: 'Robot'}
+            ]
+        },
+        alive: {
+            label: 'Is alive',
+            type: 'checkbox',
+            value: true
+        },
+        loves: {
+            label: 'Loves',
+            type: 'checkbox',
+            value: ['bike','mom','vg'],
+            options: [
+                {value: 'bike', text: 'Motorbike'},
+                {value: 'mom', text: 'His mom'},
+                {value: 'vg', text: 'Video games'},
+                {value: 'kill', text: 'Killing people'}
+            ]
+        },
+        passwd: {
+            label: 'Password',
+            type: 'password'
+        },
+        desc: {
+            label: 'Description',
+            type: 'textarea'
+        }
+    },
+    callback: function (values) {
+        console.log(values)
+    }
+})
+        }
+    }
+});</script>";
         break;
     case "first_time":
         insertNewUser($username,$password);
-        print "
-            function myFunction() {
-                var person = prompt(\"Please enter your name\", \"Harry Potter\");
-                
-                if (person != null) {
-                    document.getElementById(\"demo\").innerHTML =
-                    \"Hello \" + person + \"! How are you today?\";
-                }
-            }</script>
+        $z.="<script></script>
             ";
         break;
 }
-
+$z.="</body>
+<html>";
+print $z;
